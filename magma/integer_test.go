e447ae515b25d2647d907b992b37ae53711745e0
cocoatomo <cocoatomo77@gmail.com>
2019-05-06 20:12:55 +0900

    Integer as magma

--- magma/integer_test.go
+++ magma/integer_test.go
@@ -1,12 +1,11 @@
 package magma
 
 import (
-	"github.com/cocoatomo/goarith/magma"
 	"github.com/cocoatomo/goarith/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestMagmaIntegerOp(t *testing.T) {
-	assert.Equal(t, types.NewInteger(2), magma.Op(types.NewInteger(1), types.NewInteger(1)))
+	assert.Equal(t, types.NewInteger(2), Op(types.NewInteger(1), types.NewInteger(1)))
 }
